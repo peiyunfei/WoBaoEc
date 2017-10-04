a2ae0d951ca97a3689bec016cdf44f7e5e525fae
peiyunfei <1027282775@qq.com>
2017-10-04 21:02:56 +0800

    初始化我宝电商项目。

---
+++ Setup.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class Setup extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
